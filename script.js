c1fc5e42d6ba8fee6c6564331abe2b02c292a4cf
ViktorDojchinovski <viktorjano@yahoo.com>
2023-12-20 20:53:13 +0100

    Add OOP

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 let counter = 0;
 let colors = ["red", "green", "blue", "yellow"];
 
-// function declaration
 function onClick() {
   let n = colors.length;
   // Equvalent expressions counter = counter + 1; counter += 1;
@@ -24,3 +23,25 @@ function onReverseClick() {
   document.body.style.backgroundColor =
     colors[(index - 1 + colors.length) % colors.length];
 }
+
+//OOP variation
+function ColorChanger(colors) {
+  this.counter = 0;
+  this.colors = colors;
+}
+
+ColorChanger.prototype.init = function () {
+  changeColorBtn.addEventListener("click", this.onClick);
+};
+
+ColorChanger.prototype.onClick = function () {
+  let n = this.colors.length;
+  // Equvalent expressions counter = counter + 1; counter += 1;
+  currentColour = this.colors[this.counter % n]; // "red"
+  this.counter++;
+  document.body.style.backgroundColor = currentColour;
+};
+
+const colorChanger_1 = new ColorChanger(["red", "green", "blue", "yellow"]);
+
+const colorChanger_2 = new ColorChanger(["red", "green", "brown", "pink"]);
